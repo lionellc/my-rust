f58d309ac008924b92d874dfa7ca5748fd346727
lionellc <lionel_lc@163.com>
2025-12-12 23:48:59 +0800

    feat:【minigrep】使用迭代器来改进程序

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,17 +9,15 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = args.next().ok_or("Didn't get a query string")?;
+        let file_path = args.next().ok_or("Didn't get a file path")?;
 
         let ignore_case: bool = match env::var("IGNORE_CASE") {
             Ok(value) => value == "1",
-            Err(_) => match args.get(3) {
+            Err(_) => match args.next() {
                 Some(value) => value == "ignore",
                 None => false,
             },
@@ -83,22 +81,16 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
